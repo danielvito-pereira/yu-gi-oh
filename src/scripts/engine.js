53016e156b5101ce8e12b752424e7b9e4a664a0f
danielvito-pereira <danielvitor081@gmail.com>
2023-12-28 12:37:33 -0300

    criando os states da engine

---
+++ src/scripts/engine.js
@@ -0,0 +1,27 @@
+const sate = {
+  score:{
+    playerScore:0,
+    computerScore:0,
+    scoreBox: document.getElementById("score_points"),
+  },
+  cordSprites:{
+    avatar: document.getElementById("card-image"),
+    name: document.getElementById("card-name"),
+    type: document.getElementById("card-type"),
+  },
+  fieldCards:{
+    player:document.getElementById("player-field-card"),
+    computer:document.getElementById("computer-field-card"),
+  },
+  action:{
+    button: document.getElementById("next-duel"),
+  },
+};
+
+
+function init(){
+
+}
+
+init();
+
